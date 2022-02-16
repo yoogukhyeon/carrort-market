9bb4af7d5f3f594f170408aca943073e86ef21e9
유국현 <rnrgus5897@gmail.com>
2022-02-16 14:27:32 +0900

    tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,29 @@ import type { NextPage } from 'next'
 
 
 const Home: NextPage = () => {
-  return null
+  return (
+      <div className='bg-slate-400 py-20 px-10 grid gap-10'>
+          <div className='bg-white p-6 rounded-2xl shadow-xl'>
+                <span className='font-semibold text-3xl'>Select Item</span>
+                <div className='flex justify-between my-2'>
+                  <span className='text-gray-500'>Grey Chair</span>
+                  <span className='font-semibold'>$19</span>
+                </div>
+                <div className='flex justify-between'>
+                  <span className='text-gray-500'>Grey Chair</span>
+                  <span className='font-semibold'>$19</span>
+                </div>
+                <div className='mt-2 pt-2 border-t-2 border-dashed flex justify-between'>
+                  <span>Total</span>
+                  <span className='font-semibold'>$37</span>
+                </div>
+                <div className='my-5 bg-blue-500 text-white p-3 text-center rounded-xl w-2/4 mx-auto'>Check Out</div>
+          </div>
+          <div className='bg-white p-10 rounded-2xl shadow-xl'></div>
+          <div className='bg-white p-10 rounded-2xl shadow-xl'></div>
+          <div className='bg-white p-10 rounded-2xl shadow-xl'></div>
+      </div>
+  )
 }
 
 export default Home
